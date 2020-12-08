40c1bc962723e86face176a6f1b08c808d4daade
Bumpversion after CI <bumpversion-after-ci@example.com>
2020-12-08 20:48:00 +0000

    Bump version: 0.0.7 -> 0.0.8 [ci skip]

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="bagheera", # Replace with your own username
-    version="0.0.7",
+    version="0.0.8",
     author="Bagheera",
     author_email="bergmann.jerome@gmail.com",
     description="An implementation of the bagheera programming language",
