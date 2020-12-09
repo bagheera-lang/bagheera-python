32c6eda132a1c8ffee25979790ede2437a31fc2a
Jerome Bergmann <bergmann.jerome@gmail.com>
2020-12-09 09:33:09 +0100

    manual bumpversion

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Jerome Bergmann'
 author = 'Jerome Bergmann'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.7'
+release = '0.0.10'
 
 
 # -- General configuration ---------------------------------------------------
