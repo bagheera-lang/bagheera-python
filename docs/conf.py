ff903b6276acf76e872e5b4117513c201a76bedd
Jerome Bergmann <bergmann.jerome@gmail.com>
2020-12-09 12:26:14 +0100

    fighting with toctree part 9

--- docs/conf.py
+++ docs/conf.py
@@ -34,10 +34,12 @@ extensions = [
     'sphinx.ext.autodoc',
     # "sphinx.ext.napoleon",
     'sphinx.ext.coverage',
-    'sphinx.ext.autosummary'
+    'sphinx.ext.autosummary',
+    'sphinx.ext.viewcode'
 ]
 
 autosummary_generate = True
+autoclass_content = "both"
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
