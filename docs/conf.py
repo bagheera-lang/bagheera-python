c1a6a8927308ea7a4f944d0d1096c960c1f23a15
Bumpversion after CI <bumpversion-after-ci@example.com>
2020-12-19 10:42:45 +0000

    Bump version: 0.0.11 -> 0.0.12 [ci skip]

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Jerome Bergmann'
 author = 'Jerome Bergmann'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.11'
+release = '0.0.12'
 
 
 # -- General configuration ---------------------------------------------------
